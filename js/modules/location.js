df3b37d84ca4d1ee64bf26cc3597d17cf88abf5f
Anna Shiriaeva1 <anna_shiriaeva1@epam.com>
2020-08-12 15:14:08 +0400

    feat: add function for location set

---
+++ js/modules/location.js
@@ -0,0 +1,54 @@
+export function updateLocation(data, lang = 'en') {
+  let locationName = document.getElementsByClassName('js-location-name')[0];
+  locationName.innerText = data.city.name + ', ' + data.city.country;//tod0
+
+  let latArray = data.city.coord.lat.toString().split('.');
+  let lonArray = data.city.coord.lon.toString().split('.');
+
+  let latitude = document.getElementsByClassName('js-location-latitude')[0];
+  latitude.innerText = latArray[0] + '\xB0' + latArray[1].substr(0, 2) + '\'';
+
+  let longitude = document.getElementsByClassName('js-location-longitude')[0];
+  longitude.innerText = lonArray[0] + '\xB0' + lonArray[1].substr(0, 2) + '\'';
+
+  let iframe = document.getElementsByClassName('js-map')[0];
+  iframe.src = 'https://www.google.com/maps/embed/v1/view?center=' + data.city.coord.lat + ',' + data.city.coord.lon + '&zoom=10&key=AIzaSyBWWZnqHV3asW7DM3yCQ0dxSHjj_J9LkwE&language=' + lang;
+}
+
+
+//{ cod: "200", message: 0, cnt: 40, list: Array(40), city: { … } }
+//  city: { id: 524901, name: "Moscow", coord: { … }, country: "RU", population: 1000000, … }
+//  cnt: 40
+//  cod: "200"
+//  list: Array(40)
+//    0:
+//    clouds: { all: 70 }
+//    dt: 1597233600
+//    dt_txt: "2020-08-12 12:00:00"
+//    main:
+//      feels_like: 14.86
+//      grnd_level: 996
+//      humidity: 58
+//      pressure: 1012
+//      sea_level: 1012
+//      temp: 17.68
+//      temp_kf: -1.76
+//      temp_max: 19.44
+//      temp_min: 17.68
+//      __proto__: Object
+//    pop: 0.7
+//    rain: { 3h: 0.29 }
+//    sys: { pod: "d" }
+//    visibility: 10000
+//    weather: Array(1)
+//      0:
+//      description: "light rain"
+//      icon: "10d"
+//      id: 500
+//      main: "Rain"
+//      __proto__: Object
+//    length: 1
+//    __proto__: Array(0)
+//    wind:
+//      deg: 333
+//      speed: 3.83
--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ import { get } from './storage.js';
 import * as ipinfo from './api/ipinfo.js';
 import * as openweathermap from './api/openweathermap.js';
 
+import * as location from './modules/location.js';
+
 
 const lang = get('lang', '"en"');
 
@@ -11,8 +13,9 @@ async function f() {
   try {
     let d = await ipinfo.getIPInfo();;
     console.log(d);
-    let weather = await openweathermap.getWeatherInfo(d.city, lang)
-    console.log(weather);
+    let weatherInfo = await openweathermap.getWeatherInfo(d.city, lang)
+    console.log(weatherInfo);
+    location.updateLocation(weatherInfo);
   } catch (err) {
     // перехватит любую ошибку в блоке try: и в fetch, и в response.json
     console.log(err);
